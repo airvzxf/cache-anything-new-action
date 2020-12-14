fc138992dc363f4d8877fcaa9ccd9d393c800e8c
Israel Roldan <israel.alberto.rv@gmail.com>
2020-12-14 03:02:48 -0600

    Formatted the environment variables.

--- snapshot.sh
+++ snapshot.sh
@@ -1,10 +1,6 @@
 #!/bin/bash -e
 
-echo "GITHUB_ACTION_PATH: ${GITHUB_ACTION_PATH}"
 echo "ENV_RUNNER_TEMP:    ${ENV_RUNNER_TEMP}"
-echo "ENV_IDENTIFIER:     ${ENV_IDENTIFIER}"
-echo "ENV_SCRIPT:         ${ENV_SCRIPT}    "
-echo "ENV_VERSION:        ${ENV_VERSION}"
 echo "ENV_DIRECTORY:      ${ENV_DIRECTORY}"
 echo "ENV_EXCLUDE:        ${ENV_EXCLUDE}"
 echo "Parameter #1:       ${1}"
