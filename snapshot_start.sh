8ee48536281e0a090e0c0ab48e1b9e14561cbb04
Israel Roldan <israel.alberto.rv@gmail.com>
2020-12-14 02:14:57 -0600

    Fixed environment variables and Updated the script to run find.

--- snapshot_start.sh
+++ snapshot_start.sh
@@ -1,30 +1,26 @@
 #!/bin/bash -e
 
-echo "GITHUB_ACTION_PATH:  ${GITHUB_ACTION_PATH}"
-echo "ENV_RUNNER_TEMP:     ${ENV_RUNNER_TEMP}"
-echo "inputs.identifier:   ${ENV_IDENTIFIER}"
-echo "inputs.version:      ${ENV_VERSION}"
-echo "inputs.directory:    ${ENV_DIRECTORY}"
-echo "inputs.exclude:      ${ENV_EXCLUDE}"
+echo "GITHUB_ACTION_PATH: ${GITHUB_ACTION_PATH}"
+echo "ENV_RUNNER_TEMP:    ${ENV_RUNNER_TEMP}"
+echo "ENV_IDENTIFIER:     ${ENV_IDENTIFIER}"
+echo "ENV_VERSION:        ${ENV_VERSION}"
+echo "ENV_DIRECTORY:      ${ENV_DIRECTORY}"
+echo "ENV_EXCLUDE:        ${ENV_EXCLUDE}"
 
 FIND_INIT="sudo find"
 FIND_INIT="${FIND_INIT} ${ENV_DIRECTORY}"
 FIND_INIT="${FIND_INIT} -type f,l"
-EXCLUDED=( "${ENV_EXCLUDE}" )
+EXCLUDED=("${ENV_EXCLUDE}")
 FIND_EXCLUDE=""
-for EXCLUDE in ${EXCLUDED[*]}
-do
+for EXCLUDE in ${EXCLUDED[*]}; do
   echo "EXCLUDE: ${EXCLUDE}"
   FIND_EXCLUDE="${FIND_EXCLUDE} -not \( -path \"${EXCLUDE}*\" -prune \)"
 done
 FIND_END=" > ${ENV_RUNNER_TEMP}/system_files_snapshot_01.txt"
-FIND_END=" ${FIND_END} 2> /dev/null || true"
+FIND_END="${FIND_END} 2> /dev/null || true"
 
 FIND="${FIND_INIT}${FIND_EXCLUDE}${FIND_END}"
 echo "FIND: ${FIND}"
-#eval ${FIND}
+eval "${FIND}"
 
-echo "Hello!" > "${ENV_RUNNER_TEMP}"/system_files_snapshot_01.txt
 ls -lha "${ENV_RUNNER_TEMP}"/system_files_snapshot_01.txt
-
-#sudo find / -type f,l > ${ENV_RUNNER_TEMP}/system_files.txt 2> /dev/null || true
