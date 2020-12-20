2fc577472519b6d07fca1e2f82fd32eb7fd6bf6f
Israel Roldan <israel.alberto.rv@gmail.com>
2020-12-20 11:26:04 -0600

    Added the js extension to the cache module.

--- cache_node.sh
+++ cache_node.sh
@@ -5,8 +5,9 @@ mkdir -p /home/runner/work/_temp/actions-tool-cache/
 cd "${GITHUB_ACTION_PATH}"
 cp package.json package-lock.json cache.js /home/runner/work/_temp/actions-tool-cache/
 cd /home/runner/work/_temp/actions-tool-cache/
+ls -lha ./
 npm install
-npm run-script cache
+npm run-script cache.js
 cd -
 
 set +xv
