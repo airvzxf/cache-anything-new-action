ec8057e28e6a5270dd71584f1e446f71193540a8
Israel Roldan <israel.alberto.rv@gmail.com>
2020-12-20 11:21:11 -0600

    Remove express option in the node package install.

--- cache_node.sh
+++ cache_node.sh
@@ -5,7 +5,7 @@ mkdir -p /home/runner/work/_temp/actions-tool-cache/
 cd "${GITHUB_ACTION_PATH}"
 cp package.json package-lock.json cache.js /home/runner/work/_temp/actions-tool-cache/
 cd /home/runner/work/_temp/actions-tool-cache/
-npm install express
+npm install
 npm node cache.js
 cd -
 
