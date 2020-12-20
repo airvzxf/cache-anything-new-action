8d7fec9f53a279360606d99b7cd1b26ae7c1818e
Israel Roldan <israel.alberto.rv@gmail.com>
2020-12-20 11:19:16 -0600

    Fixed path of git actions which copy the node files.

--- cache_node.sh
+++ cache_node.sh
@@ -2,6 +2,7 @@
 
 pwd
 mkdir -p /home/runner/work/_temp/actions-tool-cache/
+cd "${GITHUB_ACTION_PATH}"
 cp package.json package-lock.json cache.js /home/runner/work/_temp/actions-tool-cache/
 cd /home/runner/work/_temp/actions-tool-cache/
 npm install express
