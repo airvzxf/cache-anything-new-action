cbb316135dd826286658752d64982bbacbb426d3
Israel Roldan <israel.alberto.rv@gmail.com>
2020-12-22 01:35:16 -0600

    Added more logs for input in the hello script.

--- src/hello.js
+++ src/hello.js
@@ -5,6 +5,10 @@ console.log(`Hello World!`);
 console.log(`*: ${core.getInput('*')}`)
 console.log(`identifier: ${core.getInput('identifier')}`)
 console.log(`inputs.identifier: ${core.getInput('inputs.identifier')}`)
+console.log(`ENV_IDENTIFIER: ${core.getInput('ENV_IDENTIFIER')}`)
+console.log(`env_identifier: ${core.getInput('env_identifier')}`)
+console.log(`*: ${core.getInput('*')}`)
+console.log(`[]: ${core.getInput('')}`)
 
 const nameToGreet = core.getInput('inputs.identifier');
 console.log(`Hello ${nameToGreet}!`);
