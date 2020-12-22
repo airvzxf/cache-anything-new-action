6f9ef571a99dc1ce98723d7a5f297a6fcd8e0c4b
Israel Roldan <israel.alberto.rv@gmail.com>
2020-12-21 22:50:57 -0600

    Fixed the async function for file cache; Added find cache logs;

--- src/cache.js
+++ src/cache.js
@@ -40,41 +40,66 @@ function cachedPathFunction() {
     try {
         const cachedPath = await cachedPathFunction();
         console.log(`cachedPath: ${cachedPath}`);
+
+        const nodeDirectory_2 = tool_cache.find('node', '12.x', 'x64');
+        core.addPath(nodeDirectory_2);
+
+        const cachedFileFind = tool_cache.find('myShellName', '1.x');
+        core.addPath(cachedFileFind);
+
+        console.log(`core.getInput -> target-install.sh: ${core.getInput('target-install.sh')}`)
+        console.log(`core.getInput -> myShellName:       ${core.getInput('myShellName')}`)
+        console.log(`core.getInput -> myShellName1.0.0:  ${core.getInput('myShellName1.0.0')}`)
+        console.log(`core.getInput -> myShellName-1.0.0: ${core.getInput('myShellName-1.0.0')}`)
+        console.log(`core.getInput -> 1.0.0:             ${core.getInput('1.0.0')}`)
+        console.log(`core.getInput -> node:       ${core.getInput('node')}`)
+        console.log(`core.getInput -> identifier: ${core.getInput('identifier')}`)
+        console.log(`core.getInput -> version:    ${core.getInput('version')}`)
+        console.log(`core.getInput -> cache:      ${core.getInput('cache')}`)
+        console.log(`core.getInput -> script:     ${core.getInput('script')}`)
+        console.log(`core.getInput -> snapshot:   ${core.getInput('snapshot')}`)
+        console.log(`core.getInput -> exclude:    ${core.getInput('exclude')}`)
+        console.log(`core.getState -> #01: ${core.getState('CACHE_KEY')}`)
+        console.log(`core.getState -> #02: ${core.getState('CACHE_RESULT')}`)
     } catch (e) {
         console.error(`Error in cachedPath: ${e}`)
     }
 })();
 
-async function cachedFileFunction() {
+function cachedFileFunction() {
     const cachedFile = tool_cache.cacheFile('/home/runner/work/testing-actions-github/testing-actions-github/.github/workflows/install.sh', 'target-install.sh', 'myShellName', '1.0.0');
     core.addPath(cachedFile);
     console.log(`cachedFile: ${cachedFile}`)
     return cachedFile;
 }
 
-try {
-    const cachedFile = cachedFileFunction();
-    console.log(`cachedFile: ${cachedFile}`)
-} catch (e) {
-    console.error(`Error in cachedFile: ${e}`)
-}
-const nodeDirectory_2 = tool_cache.find('node', '12.x', 'x64');
-core.addPath(nodeDirectory_2);
+(async () => {
 
-const cachedFileFind = tool_cache.find('myShellName', '1.x');
-core.addPath(cachedFileFind);
+    try {
+        const cachedFile = await cachedFileFunction();
+        console.log(`cachedFile: ${cachedFile}`)
 
-console.log(`core.getInput -> target-install.sh: ${core.getInput('target-install.sh')}`)
-console.log(`core.getInput -> myShellName:       ${core.getInput('myShellName')}`)
-console.log(`core.getInput -> myShellName1.0.0:  ${core.getInput('myShellName1.0.0')}`)
-console.log(`core.getInput -> myShellName-1.0.0: ${core.getInput('myShellName-1.0.0')}`)
-console.log(`core.getInput -> 1.0.0:             ${core.getInput('1.0.0')}`)
-console.log(`core.getInput -> node:       ${core.getInput('node')}`)
-console.log(`core.getInput -> identifier: ${core.getInput('identifier')}`)
-console.log(`core.getInput -> version:    ${core.getInput('version')}`)
-console.log(`core.getInput -> cache:      ${core.getInput('cache')}`)
-console.log(`core.getInput -> script:     ${core.getInput('script')}`)
-console.log(`core.getInput -> snapshot:   ${core.getInput('snapshot')}`)
-console.log(`core.getInput -> exclude:    ${core.getInput('exclude')}`)
-console.log(`core.getState -> #01: ${core.getState('CACHE_KEY')}`)
-console.log(`core.getState -> #02: ${core.getState('CACHE_RESULT')}`)
+        const nodeDirectory_2 = tool_cache.find('node', '12.x', 'x64');
+        core.addPath(nodeDirectory_2);
+
+        const cachedFileFind = tool_cache.find('myShellName', '1.x');
+        core.addPath(cachedFileFind);
+
+        console.log(`core.getInput -> target-install.sh: ${core.getInput('target-install.sh')}`)
+        console.log(`core.getInput -> myShellName:       ${core.getInput('myShellName')}`)
+        console.log(`core.getInput -> myShellName1.0.0:  ${core.getInput('myShellName1.0.0')}`)
+        console.log(`core.getInput -> myShellName-1.0.0: ${core.getInput('myShellName-1.0.0')}`)
+        console.log(`core.getInput -> 1.0.0:             ${core.getInput('1.0.0')}`)
+        console.log(`core.getInput -> node:       ${core.getInput('node')}`)
+        console.log(`core.getInput -> identifier: ${core.getInput('identifier')}`)
+        console.log(`core.getInput -> version:    ${core.getInput('version')}`)
+        console.log(`core.getInput -> cache:      ${core.getInput('cache')}`)
+        console.log(`core.getInput -> script:     ${core.getInput('script')}`)
+        console.log(`core.getInput -> snapshot:   ${core.getInput('snapshot')}`)
+        console.log(`core.getInput -> exclude:    ${core.getInput('exclude')}`)
+        console.log(`core.getState -> #01: ${core.getState('CACHE_KEY')}`)
+        console.log(`core.getState -> #02: ${core.getState('CACHE_RESULT')}`)
+    } catch (e) {
+        console.error(`Error in cachedFile: ${e}`)
+    }
+})();
