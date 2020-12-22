349c3cfb438a499346da92958a87d54cf475e02a
Israel Roldan <israel.alberto.rv@gmail.com>
2020-12-21 23:53:16 -0600

    Fixed restore cache function.

--- src/cache.js
+++ src/cache.js
@@ -3,19 +3,19 @@ const cache = require('@actions/cache');
 
 console.log(`core.isDebug(): ${core.isDebug()}`)
 
+const key = 'hello-v1.0.1'
+const restoreKeys = ['hello-']
 const paths = [
     '/home/runner/work/testing-actions-github/testing-actions-github/.github/workflows/hello.txt'
 ]
-const key = 'hello-v1.0.1'
-const restoreKeys = ['hello-']
 
 function restoreCache(paths, key, restoreKeys) {
-    return cache.saveCache(paths, key, restoreKeys)
+    return cache.restoreCache(paths, key, restoreKeys)
 }
 
 (async () => {
     try {
-        const cacheId = await restoreCache(paths, key, restoreKeys)
+        const cacheId = await restoreCache(paths, key, '')
         console.log(`cacheID: ${cacheId}`)
     } catch (error) {
         console.error(`Error in restoreCache #1: ${error}`)
@@ -28,17 +28,17 @@ function saveCache(paths, key) {
 
 (async () => {
     try {
-        const cacheKey = core.getState("CACHE_RESULT");
-        if (cacheKey) {
-            core.debug(`Cache state/key: ${cacheKey}`);
-        } else {
-            console.error(`Not Cache Result found.`)
-            return;
-        }
-        if (cacheKey && cacheKey.localeCompare(key, undefined, {sensitivity: "accent"}) === 0) {
-            console.error(`Cache hit occurred on the primary key ${key}, not saving cache.`);
-            return;
-        }
+        // const cacheKey = core.getState("CACHE_RESULT");
+        // if (cacheKey) {
+        //     core.debug(`Cache state/key: ${cacheKey}`);
+        // } else {
+        //     console.error(`Not Cache Result found.`)
+        //     return;
+        // }
+        // if (cacheKey && cacheKey.localeCompare(key, undefined, {sensitivity: "accent"}) === 0) {
+        //     console.error(`Cache hit occurred on the primary key ${key}, not saving cache.`);
+        //     return;
+        // }
         const cacheId = await saveCache(paths, key)
         console.log(`cacheID: ${cacheId}`)
     } catch (error) {
