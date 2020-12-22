838f46c1c4f301b8a429d656320ec07a90e3bd6d
Israel Roldan <israel.alberto.rv@gmail.com>
2020-12-22 01:14:47 -0600

    Added the original action cache files.

---
+++ src/save.ts
@@ -0,0 +1,63 @@
+import * as cache from "@actions/cache";
+import * as core from "@actions/core";
+
+import {Events, Inputs, State} from "./constants";
+import * as utils from "./utils/actionUtils";
+
+async function run(): Promise<void> {
+    try {
+        if (utils.isGhes()) {
+            utils.logWarning("Cache action is not supported on GHES");
+            return;
+        }
+
+        if (!utils.isValidEvent()) {
+            utils.logWarning(
+                `Event Validation Error: The event type ${
+                    process.env[Events.Key]
+                } is not supported because it's not tied to a branch or tag ref.`
+            );
+            return;
+        }
+
+        const state = utils.getCacheState();
+
+        // Inputs are re-evaluted before the post action, so we want the original key used for restore
+        const primaryKey = core.getState(State.CachePrimaryKey);
+        if (!primaryKey) {
+            utils.logWarning(`Error retrieving key from state.`);
+            return;
+        }
+
+        if (utils.isExactKeyMatch(primaryKey, state)) {
+            console.info(
+                `Cache hit occurred on the primary key ${primaryKey}, not saving cache.`
+            );
+            return;
+        }
+
+        const cachePaths = utils.getInputAsArray(Inputs.Path, {
+            required: true
+        });
+
+        try {
+            await cache.saveCache(cachePaths, primaryKey, {
+                uploadChunkSize: utils.getInputAsInt(Inputs.UploadChunkSize)
+            });
+        } catch (error) {
+            if (error.name === cache.ValidationError.name) {
+                throw error;
+            } else if (error.name === cache.ReserveCacheError.name) {
+                console.info(error.message);
+            } else {
+                utils.logWarning(error.message);
+            }
+        }
+    } catch (error) {
+        utils.logWarning(error.message);
+    }
+}
+
+run();
+
+export default run;
