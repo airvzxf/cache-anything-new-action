d946d6b5a15f29ca4dbd7f79fe65514669ea5c59
Israel Roldan <israel.alberto.rv@gmail.com>
2020-12-21 23:03:04 -0600

    Removed the display files for node app.

--- cache_node.sh
+++ cache_node.sh
@@ -5,11 +5,6 @@ mkdir --parents "${ENV_RUNNER_TEMP}"/actions-tool-cache/
 cd "${GITHUB_ACTION_PATH}"
 cp --recursive ./* "${ENV_RUNNER_TEMP}"/actions-tool-cache/
 cd "${ENV_RUNNER_TEMP}"/actions-tool-cache/
-ls -lha /opt/hostedtoolcache/node/
-ls -lha /opt/hostedtoolcache/node/14.15.1/
-ls -lha /opt/hostedtoolcache/node/14.15.1/x64
-ls -lha /opt/hostedtoolcache/node/12.20.0/
-ls -lha /opt/hostedtoolcache/node/12.20.0/x64
 ls -lha ./
 npm install --scripts-prepend-node-path=auto
 npm run build --scripts-prepend-node-path=auto
